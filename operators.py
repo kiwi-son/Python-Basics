b56bb1b608e55eecf4e22f2aa0a14640e67fc0df
Jeet Sadhukhan <jeet.sadhukhan9.2@gmail.com>
2025-10-23 21:45:40 +0530

    Update operators.py
    Comparison operators & logical operators added

--- operators.py
+++ operators.py
@@ -8,4 +8,20 @@ print("a * b =",a*b) #Multiplication
 print("a / b =",a/b) #Division
 print("a // b =",a//b) #Floor Division
 print("a % b =",a%b) #Modulus
+
 print("a ** b =",a**b) #Exponentiation
+
+#Comparison Operators Module
+print(10>6) #greater than
+print(10<6) #less than
+print(10==6) #equal
+print(10!=6) #not equal
+print(10>=6) #greater than or equal to
+print(10<=6) #less than or equal to
+
+#Logical Operators Module
+x = True
+y = False
+print(x and y) #logical AND
+print(x or y)  #logical OR  
+print(not x)    #logical NOT
