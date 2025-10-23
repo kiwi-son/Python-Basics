d56f283a04f7aa7af6e5749ded51ca7b5b88f925
Jeet Sadhukhan <jeet.sadhukhan9.2@gmail.com>
2025-10-23 12:20:25 +0530

    Add files via upload
    Learn about different types of arithmetic operators in Python.

---
+++ operators.py
@@ -0,0 +1,11 @@
+#REPL:- READ EVALUATE -PRINT LOOP
+#Arithmetic Operators Module
+a=220
+b=15
+print("a + b =",a+b) #Addition
+print("a - b =",a-b) #Subtraction
+print("a * b =",a*b) #Multiplication
+print("a / b =",a/b) #Division
+print("a // b =",a//b) #Floor Division
+print("a % b =",a%b) #Modulus
+print("a ** b =",a**b) #Exponentiation
