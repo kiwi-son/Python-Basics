ee1f1d47edd923a1e310f1fae7fe9e0e9204e34d
Jeet Sadhukhan <jeet.sadhukhan9.2@gmail.com>
2025-10-24 21:02:14 +0530

    Add files via upload
    Basic of strings are added

---
+++ strings.py
@@ -0,0 +1,16 @@
+name='''jeet'''
+print(name)
+c="""
+This is a multi-line string.
+"""
+print(c)#tripple quotes can be used to create multi-line strings
+d='''This is also a
+multi-line string.'''
+print(d)
+
+name="Jeet"
+print(name[0])#prints first character
+print(name[1])#prints second character
+print(name[-1])
+print(name[-4])
+
---
+++ strings_slicing.py
@@ -0,0 +1,8 @@
+name="Soumik"
+print(name[0:2])#prints characters from index 0 to 2-1
+print(name[2:-1]) #print characters from index 2 to len(name)-1
+print(name[::-1]) #prints the string in reverse order
+print(name[::2]) #skip 2-1 characters and print
+print(name[1::2]) #prints every second character starting from index 1
+print(name[::-2])
+print(name[-6:-1]) #prints last 5 characters in reverse order
